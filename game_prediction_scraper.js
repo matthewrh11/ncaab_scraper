19bd38ea78334ab0d562b284b73e64a04bc164ec
Matthew Hiscock <mrh470@mun.ca>
2022-02-18 15:29:03 -0330

    first commit

---
+++ game_prediction_scraper.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const rp = require('request-promise');
+const csv = require('csv-parser')
+const url = 'https://raw.githubusercontent.com/lbenz730/NCAA_Hoops/master/3.0_Files/Predictions/predictions.csv';
+
+function getDateString(){
+    let yourDate = new Date();
+
+    const tzOffset = yourDate.getTimezoneOffset();
+    yourDate = new Date(yourDate.getTime() - (tzOffset * 60 * 1000));
+
+    return yourDate.toISOString().split('T')[0];
+}
+
+function generateGameData() {
+    const results = []
+
+    rp(url).then((csv) => {
+        fs.writeFileSync("scraped_data.csv", csv);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+    fs.createReadStream('./scraped_data.csv')
+        .pipe(csv())
+        .on('data', (data) => {
+            if (getDateString() === data.date && !results.find((result) => result.opponent === data.team )) {
+                results.push(data)
+            }
+        })
+        .on('end', () => {
+            fs.writeFileSync("game_data.json", JSON.stringify(results));
+            console.log(results.length);
+        });
+}
+
+generateGameData();
